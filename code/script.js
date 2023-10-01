dd2d0eb1124f1d10e3cb24722fd6832fb1da6662
Emma Olsson <emma.olsson22@learnet.se>
2023-10-01 23:57:35 +0200

    added aubtype choice

--- code/script.js
+++ code/script.js
@@ -39,7 +39,41 @@ alert(`${selectedFood} is a good choice!`);
 
 // Step 3 - Subtype choice
 
+let subtypeFoodChoice;
 
+switch (foodChoice) {
+  case '1':
+    subtypeFoodChoice = prompt(
+      `What ${selectedFood} are you having today?
+        1 - Funghi
+        2 - Vesuvio
+        3 - Hawaii
+      Select by entering a number!`
+    );
+    break;
+  case '2':
+    subtypeFoodChoice = prompt(
+      `What ${selectedFood} are you having today?
+        1 - Bolognese
+        2 - Arrabiata
+        3 - Carbonara
+      Select by entering a number!`
+    );
+    break;
+  case '3':
+    subtypeFoodChoice = prompt(
+      `What ${selectedFood} are you having today?
+        1 - Burrata
+        2 - Caesar
+        3 - Caprese
+      Select by entering a number!`
+    );
+    break;
+  default:
+    alert('Incorrect value!');
+}
+
+alert(`${subtypeFoodChoice} is an excellent choice!`);
 
 // Step 4 - Age
 // Your code goes here
