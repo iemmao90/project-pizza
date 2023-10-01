3a50fbe8a6b1c1efbfbfb152dea289e114227f08
Emma Olsson <emma.olsson22@learnet.se>
2023-10-01 21:39:00 +0200

    Added food choice

--- code/script.js
+++ code/script.js
@@ -1,20 +1,27 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 );
 
 let username = prompt("What's your name?");
 
-alert(`Hi and welcome ${username}`);
+alert(`Hi and welcome ${username}!`);
 
 // Step 2 - Food choice
-// Your code goes here
+let foodChoice = prompt(`What are you having today?
+
+1 - Pizza
+2 - Pasta
+3 - Salad
+
+Select by entering a number!`);
+
+alert(`${foodChoice} is a good choice!`);
 
 // Step 3 - Subtype choice
-// Your code goes here
+
 
 // Step 4 - Age
 // Your code goes here
